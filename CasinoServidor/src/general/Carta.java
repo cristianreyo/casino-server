e1f0c294f8024ee235cb14bdf08e686e5d09a5d6
18fernan@gmail.com <18fernan@gmail.com@222511fb-cf35-9cb0-c158-666a0d031e6f>
2011-12-21 02:05:26 +0000

    Añadida la carpeta con las imagenes de las cartas Añadido el servidor para el poker (falta implementar el nucleo del juego, las comunicaciones funcionan perfectamente) Añadida la clase carta y baraja en el paquete general

---
+++ CasinoServidor/src/general/Carta.java
@@ -0,0 +1,105 @@
+package general;
+
+import java.io.Serializable;
+import javax.swing.ImageIcon;
+
+public class Carta implements Serializable {
+	private static final long serialVersionUID=1;
+	private String numero; //El numero de la carta
+	private String palo; //El palo de la carta
+	private String color; //El color de la carta
+	private ImageIcon imagen; //Imagen de la carta
+	
+
+
+	/**
+	 * Metodo constructor en el que se indican los atributos de la carta
+	 * @param numero. Numero que tendra la carta. Puede ser del 1 al 10 o j, q, k
+	 * @param palo. Indica el palo de la carta, puede ser treboles, rombos, corazones o picas
+	 * @param color. Indica el color de la carta. puede ser rojo o negro.
+	 */
+	public Carta(String numero, String palo, String color, ImageIcon imagen){
+		this.numero=numero;
+		this.palo=palo;
+		this.color=color;
+		this.imagen=imagen;
+	}
+	
+	/**
+	 * Metodo para obtener el numero de la carta
+	 * @return numero. Es el numero de la carta
+	 */
+	public String getNumero() {
+		return numero;
+	}
+	
+	/**
+	 * Permite asignarle un numero a una carta
+	 * @param numero. Es el nuevo numero que tendrá la carta.
+	 */
+	public void setNumero(String numero) {
+		if(numero.compareTo("UNO")==0||numero.compareTo("DOS")==0||numero.compareTo("TRES")==0||numero.compareTo("CUATRO")==0||
+		   numero.compareTo("CINCO")==0||numero.compareTo("SEIS")==0||numero.compareTo("SIETE")==0||numero.compareTo("OCHO")==0||
+		   numero.compareTo("NUEVE")==0||numero.compareTo("DIEZ")==0||numero.compareTo("J")==0||numero.compareTo("Q")==0||numero.compareTo("K")==0){
+			this.numero = numero;
+		}
+	}
+	
+	/**
+	 * Metodo para obtener el palo de una carta
+	 * @return palo. Es el palo de la carta
+	 */
+	public String getPalo() {
+		return palo;
+	}
+	
+	/**
+	 * Permite modificar el palo de una carta.
+	 * @param palo. Es el nuevo valor del palo de la carta
+	 */
+	public void setPalo(String palo) {
+		if(palo.compareTo("PICAS")==0||palo.compareTo("CORAZONES")==0||
+		   palo.compareTo("TREBOLES")==0||palo.compareTo("ROMBOS")==0){
+			this.palo = palo;
+		}
+	}
+	
+	/**
+	 * Metodo para obtener el color de una carta
+	 * @return color. Es el color de la carta
+	 */
+	public String getColor() {
+		return color;
+	}
+	
+	/**
+	 * Permite modificar el color de la carta
+	 * @param color. Es el nuevo color de la carta
+	 */
+	public void setColor(String color) {
+	if(color.compareTo("ROJO")==0||color.compareTo("NEGRO")==0)
+		this.color = color;
+	}
+	/**
+	 * Metodo que devuelve un objeto de tipo imagen
+	 * @return imagen. Es la imagen asociada a una carta
+	 */
+	public ImageIcon getImagen() {
+		return imagen;
+	}
+	
+	/**
+	 * Metodo que permite asignarle una imagen a una carta.
+	 * @param imagen. Es la imagen asociada a la carta
+	 */
+	public void setImagen(ImageIcon imagen) {
+		this.imagen = imagen;
+	}
+	/**
+	 * Metodo que devuelve el ID de la clase. Se utiliza para serializar.
+	 * @return
+	 */
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+}
---
+++ CasinoServidor/src/pruebaPoker_servidor/Poker.java
@@ -0,0 +1,113 @@
+package pruebaPoker_servidor;
+
+import general.Carta;
+
+import java.util.*;
+import javax.swing.*;
+
+public class Poker {
+
+	public static boolean escaleraReal(Jugador gamer, Vector<Carta> mesa) {
+		Vector<Carta> todasCartas=mesa;
+		todasCartas.add(gamer.getCarta1());
+		todasCartas.add(gamer.getCarta2());
+		Carta c1=new Carta("UNO", "PICAS","NEGRO",new ImageIcon("imagenes/1-Picas.jpg"));
+		Carta c2=new Carta("DIEZ", "PICAS","NEGRO",new ImageIcon("imagenes/10-Picas.jpg"));
+		Carta c3=new Carta("J", "PICAS","NEGRO",new ImageIcon("imagenes/J-Picas.jpg"));
+		Carta c4=new Carta("Q", "PICAS","NEGRO",new ImageIcon("imagenes/Q-Picas.jpg"));
+		Carta c5=new Carta("K", "PICAS","NEGRO",new ImageIcon("imagenes/K-Picas.jpg"));
+		
+		Carta c6=new Carta("UNO", "CORAZONES","ROJO",new ImageIcon("imagenes/1-Corazones.jpg"));
+		Carta c7=new Carta("DIEZ", "CORAZONES","ROJO",new ImageIcon("imagenes/10-Corazones.jpg"));
+		Carta c8=new Carta("J", "CORAZONES","ROJO",new ImageIcon("imagenes/J-Corazones.jpg"));
+		Carta c9=new Carta("Q", "CORAZONES","ROJO",new ImageIcon("imagenes/Q-Corazones.jpg"));
+		Carta c10=new Carta("K", "CORAZONES","ROJO",new ImageIcon("imagenes/K-Corazones.jpg"));
+		
+		Carta c11=new Carta("UNO", "TREBOLES", "NEGRO",new ImageIcon("imagenes/1-Treboles.jpg"));
+		Carta c12=new Carta("DIEZ", "TREBOLES", "NEGRO",new ImageIcon("imagenes/10-Treboles.jpg"));
+		Carta c13=new Carta("J", "TREBOLES", "NEGRO",new ImageIcon("imagenes/J-Treboles.jpg"));
+		Carta c14=new Carta("Q", "TREBOLES", "NEGRO",new ImageIcon("imagenes/Q-Treboles.jpg"));
+		Carta c15=new Carta("K", "TREBOLES", "NEGRO",new ImageIcon("imagenes/K-Treboles.jpg"));
+		
+		Carta c16=new Carta("UNO", "ROMBOS", "ROJO",new ImageIcon("imagenes/1-Rombos.jpg"));
+		Carta c17=new Carta("DIEZ", "ROMBOS", "ROJO",new ImageIcon("imagenes/10-Rombos.jpg"));
+		Carta c18=new Carta("J", "ROMBOS", "ROJO",new ImageIcon("imagenes/J-Rombos.jpg"));
+		Carta c19=new Carta("Q", "ROMBOS", "ROJO",new ImageIcon("imagenes/Q-Rombos.jpg"));
+		Carta c20=new Carta("K", "ROMBOS", "ROJO",new ImageIcon("imagenes/K-rombos.jpg"));
+		
+		if(todasCartas.contains(c1)&&todasCartas.contains(c2)&&todasCartas.contains(c3)&&todasCartas.contains(c4)&&todasCartas.contains(c5))
+			return true;
+		else if(todasCartas.contains(c6)&&todasCartas.contains(c7)&&todasCartas.contains(c8)&&todasCartas.contains(c9)&&todasCartas.contains(c10))
+			return true;
+		else if(todasCartas.contains(c11)&&todasCartas.contains(c12)&&todasCartas.contains(c13)&&todasCartas.contains(c14)&&todasCartas.contains(c15))
+			return true;
+		else if(todasCartas.contains(c16)&&todasCartas.contains(c17)&&todasCartas.contains(c18)&&todasCartas.contains(c19)&&todasCartas.contains(c20))
+			return true;
+		else
+			return false;
+	}
+
+	public static boolean escaleraColor(Jugador gamer, Vector<Carta> mesa) {
+		Vector<Carta> todasCartas=mesa;
+		todasCartas.add(gamer.getCarta1());
+		todasCartas.add(gamer.getCarta2());
+		return false;
+	}
+
+	public static boolean poker(Jugador gamer, Vector<Carta> mesa) {
+		Vector<Carta> todasCartas=mesa;
+		todasCartas.add(gamer.getCarta1());
+		todasCartas.add(gamer.getCarta2());
+		return false;
+	}
+
+	public static boolean full(Jugador gamer, Vector<Carta> mesa) {
+		Vector<Carta> todasCartas=mesa;
+		todasCartas.add(gamer.getCarta1());
+		todasCartas.add(gamer.getCarta2());
+		return false;
+	}
+
+	public static boolean color(Jugador gamer, Vector<Carta> mesa) {
+		Vector<Carta> todasCartas=mesa;
+		todasCartas.add(gamer.getCarta1());
+		todasCartas.add(gamer.getCarta2());
+		return false;
+	}
+
+	public static boolean escalera(Jugador gamer, Vector<Carta> mesa) {
+		Vector<Carta> todasCartas=mesa;
+		todasCartas.add(gamer.getCarta1());
+		todasCartas.add(gamer.getCarta2());
+		return false;
+	}
+
+	public static boolean trio(Jugador gamer, Vector<Carta> mesa) {
+		Vector<Carta> todasCartas=mesa;
+		todasCartas.add(gamer.getCarta1());
+		todasCartas.add(gamer.getCarta2());
+		return false;
+	}
+
+	public static boolean doblesParejas(Jugador gamer, Vector<Carta> mesa) {
+		Vector<Carta> todasCartas=mesa;
+		todasCartas.add(gamer.getCarta1());
+		todasCartas.add(gamer.getCarta2());
+		return false;
+	}
+
+	public static boolean pareja(Jugador gamer, Vector<Carta> mesa) {
+		Vector<Carta> todasCartas=mesa;
+		todasCartas.add(gamer.getCarta1());
+		todasCartas.add(gamer.getCarta2());
+		return false;
+	}
+
+	public static boolean cartaAlta(Jugador gamer, Vector<Carta> mesa) {
+		Vector<Carta> todasCartas=mesa;
+		todasCartas.add(gamer.getCarta1());
+		todasCartas.add(gamer.getCarta2());
+		return false;
+	}
+
+}
